ca1b144fd1798238f71163af91d61f4482835b73
蔣政燁 <cathay2218@gmail.com>
2021-03-22 17:25:56 +0800

    20210322-1
    1. Add OpenCV_Note.txt
    2. ColorPick / ColorDetect Edit

--- ColorDetect.py
+++ ColorDetect.py
@@ -1,32 +1,47 @@
 import cv2
 import numpy
+import argparse
 
-image = cv2.imread(r"D:\Python\GitHub\ImageRecognize\Sample\HeatImageTest.jpg")
+#Argument Input via Command Prompt
+parser = argparse.ArgumentParser()
+parser.add_argument('-input', type = str, help = 'Image Path for Detect Color')
+parser.add_argument('-lowerColor', type = str, help = 'Lower Detect Color Bound [B, G, R]')
+parser.add_argument('-upperColor', type = str, help = 'Upper Detect Color Bound [B, G, R]')
 
-#偵測顏色範圍
+arg = parser.parse_args()
+print (arg)
+
+#=====================================================================================
+
+#image = cv2.imread(arg.input)
+image = cv2.imread(r"D:\ImageRecognize\Github\Sample\HeatImageTest.jpg")                #for company path
+#image = cv2.imread(r"D:\Python\GitHub\ImageRecognize\Sample\HeatImageTest.jpg")        #for home path
+
+#要偵測的顏色範圍
 lower = numpy.array([37, 174, 201])
 upper = numpy.array([77, 274, 301])
 
-#以HSV色彩空間模型抓取目標影像顏色範圍(輸出黑白遮罩圖)
+#以HSV色彩空間模型抓取目標影像顏色範圍(簡化輸出為黑白遮罩二值圖)
 filtered = cv2.inRange(image, lower, upper)
 cv2.imshow('color mask_HSV Color Space', filtered)
 
-#高斯模糊
+#將HSV色彩空間黑白遮罩二值圖進行高斯模糊處理
+#此為必須處理項目, 如未經過高斯模糊處理則輪廓搜索結果數會大幅上升且範圍會變零碎
 blurred = cv2.GaussianBlur(filtered, (15, 15), 0)
 cv2.imshow('color mask_GaussianBlur', blurred)
 
-# find contours in the image
-(cnts, _) = cv2.findContours(blurred.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-print ("Matches Area: " + str(len(cnts)))
+#尋找目標輪廓
+(cnts, _) = cv2.findContours(blurred.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)    #輸入影像為高斯模糊的副本
+print ("Matches Area Counts: " + str(len(cnts)))
 
-# 取全區域
-for cnt in sorted(cnts, key = cv2.contourArea, reverse = True):
+#取全區域輪廓
+for cnt in cnts:
     rect = numpy.int32(cv2.boxPoints(cv2.minAreaRect(cnt)))
-    cv2.drawContours(image, [rect], -1, (0, 255, 0), 2)
+    cv2.drawContours(image, [rect], -1, (0, 255, 0), 2)     #描繪輪廓
 
-#取單一區域
-# if len(cnts) > 0:
-#     cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+#取單一輪廓
+# if len(cnts) > 1:
+#     cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]      #Sort by Contour Area, Ordey by DESC (reverse = True)
 
 # # compute the (rotated) bounding box around then
 # # contour and then draw it
@@ -35,6 +50,6 @@ for cnt in sorted(cnts, key = cv2.contourArea, reverse = True):
 
 
 #Show Image and Wait Any Key to Exit Program
-cv2.imshow("Tracking", image)
+cv2.imshow("Color Tracking", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- ColorPick.py
+++ ColorPick.py
@@ -1,6 +1,6 @@
-import argparse
 import cv2
 import numpy
+import argparse
 
 #Sub Function: 滑鼠左鍵KeyDownEvent
 def pick_color(event, x, y, flags, param):
@@ -13,27 +13,34 @@ def pick_color(event, x, y, flags, param):
         print ("OriginalColor, Color Low Bound, Color Up Bound")
         print (pixelColor, lower, upper)
 
-        #以HSV色彩空間模型抓取目標影像顏色範圍(輸出黑白遮罩圖)
-        image_mask = cv2.inRange(image,lower,upper)
+        #以HSV色彩空間模型抓取目標影像顏色範圍(輸出黑白遮罩二值圖)
+        #image_mask = cv2.inRange(image,lower,upper)
         #cv2.imshow("mask_HSV Color Space",image_mask)
 
-#----------------------------------------------------------------------------------------------------
+#=====================================================================================
 
+#Argument Input via Command Prompt
 parser = argparse.ArgumentParser()
-parser.add_argument('--source', type = str, default='aaaaaa', help='Sample Image for Detect Color')
+parser.add_argument('-source', type = str, help = 'Image for Select Color')
 arg = parser.parse_args()
-print (arg.source)
+print (arg)
 
-
+#=====================================================================================
 
 #Main Function
-image = cv2.imread(r"D:\Python\GitHub\ImageRecognize\Sample\HeatImage.jpg") #320 x 240 px
+if arg.source == None:
+    print ("Default Input")
+    image = cv2.imread(r"D:\ImageRecognize\Github\Sample\HeatImage.jpg") #320 x 240 px @Company Path
+    #image = cv2.imread(r"D:\Python\GitHub\ImageRecognize\Sample\HeatImage.jpg") #320 x 240 px @Home Path
+else:
+    print ("Argument Input")
+    image = cv2.imread(arg.source)
 
 #Show Image and Set Mouse Key Down Event
 cv2.namedWindow('Image')
 cv2.setMouseCallback('Image', pick_color)
 cv2.imshow("Image",image)
 
-#Wait Next Mouse Key Down Event or Any Key to Exit Program
+#Wait Mouse Key Down Event or Any Key to Exit Program
 cv2.waitKey(0)
 cv2.destroyAllWindows()
